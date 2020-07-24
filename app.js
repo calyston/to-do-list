9aed1faf3f795bd2e4850af5f46186c75fabfa64
unknown <calyston.14@gmail.com>
2020-07-24 13:22:21 -0400

    feat: added to do div and to do item into add to do function

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
+
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 
 //Functions
 function addTodo(event) {
   event.preventDefault();
-  console.log('Hello');
+  //To Do Div for the item and buttons
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  //To Do Item
+  const newTodo = document.createElement('li');
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
+  console.log(todoInput.value);
+  todoDiv.appendChild(newTodo);
 }
