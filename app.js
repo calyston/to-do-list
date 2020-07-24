7dacc8ba1af250c73fe12ea31d0701df2686fcf6
unknown <calyston.14@gmail.com>
2020-07-24 13:12:32 -0400

    feat: added event listener to to do button, prevent default

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
+todoButton.addEventListener('click', addTodo);
 
 //Functions
+function addTodo(event) {
+  event.preventDefault();
+  console.log('Hello');
+}
